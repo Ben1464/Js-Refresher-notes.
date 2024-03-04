f71f9887c373807a2cbd64c45e470433422d33eb
Benson Mwangi <kimaniben@gmail.com>
2024-03-04 19:47:11 +0000

    Conditional statements

--- script.js
+++ script.js
@@ -255,6 +255,77 @@ ages.forEach(function(ages, index) {
    return total + numbers5;
  });
  console.log(summation);
+ //Dates in javascript
+ const date = new Date();
+ console.log(date);
+ //Math object 
+ const pi = Math.PI;
+ console.log(pi);
+ //1 Math.round(x)
+ let height = 4.8;
+ let roundedNumber = Math.round(height)
+ console.log(roundedNumber);
+ //2 Math.ceil(x)
+ let height2 =6.3;
+ let roundedNumber2 = Math.ceil(height2)
+ console.log(roundedNumber2);
+ //3 Math.floor(x)
+ let height3 =6.3;
+ let roundedNumber3 = Math.floor(height3)
+ console.log(roundedNumber3);
+ //4 Math.trunc(x)
+ let height4 =6.3;
+ let roundedNumber4 = Math.trunc(height4)
+ console.log(roundedNumber4);
+ //5 Math.sign(x)
+ let height5 =6.3;
+ let roundedNumber5 = Math.sign(height5)
+ console.log(roundedNumber5);
+ //6 Math.pow(x,y)
+ let height6 =6.3;
+ let roundedNumber6 = Math.pow(height6,2)
+ console.log(roundedNumber6);
+ //7 Math.sqrt(x)
+ let height7 =6.3;
+ let roundedNumber7 = Math.sqrt(height7)
+ console.log(roundedNumber7);
+ //8 Math.abs(x)
+ let height8 =6.3;
+ let roundedNumber8 = Math.abs(height8)
+ console.log(roundedNumber8);
+ //Conditional statements 
+ let age3 = 18;
+ if (age >= 18) {
+   console.log("I can vote");
+ } else {
+   console.log("I can't vote");
+ }
+ //conditional statements using else if
+ let age4 = 18;
+ if (age >= 18) {
+   console.log("I can vote");
+ } else if (age >= 16) {
+   console.log("I can drive");
+ } else if (age >= 14) {
+   console.log("I can go to school");
+ } else {
+   console.log("I can't do anything");
+ }
+ //COnditional statements using switch
+ let age5 = 18;
+ switch (age5) {
+   case 18:
+     console.log("I can vote");
+     break;
+   case 16:
+     console.log("I can drive");
+     break;
+   case 14:
+     console.log("I can go to school");
+     break;
+   default:
+     console.log("I can't do anything");
+ }
 
  
 
